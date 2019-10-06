ba0619101d9ed41f6047dd5323da2f830f620037
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 03:02:32 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,11 @@
 def current_age_for_birth_year(birth_year) 
   current_year = 2003
   age = current_year - birth_year
-  print "Age is: " , age
+  puts "Age is: " , age
   return age
 end  
 
 current_age_for_birth_year(1984)
 
+ age_of_person = current_age_for_birth_year(1984)
+expect(age_of_person).to eq(19)
