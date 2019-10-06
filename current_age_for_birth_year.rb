d16af17781a20fdc9861283404ee6c3520b0b6a0
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 02:47:10 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -3,7 +3,7 @@ current_year = 2003
 def current_age_for_birth_year(birth_year) 
   puts "barf " #{birth_year}
   puts = current_year  - #{birth_year}
-  return age
+
 end  
 
 current_age_for_birth_year(1984)
